650aac32d066ff114858d6dd24893215bc6187ac
Samuel Braun <slb2250@columbia.edu>
2023-08-17 23:03:08 -0400

    restored pinyin feature

--- chinese_chat_pal.py
+++ chinese_chat_pal.py
@@ -1,13 +1,25 @@
 from flask import Flask, render_template, request, jsonify
 import openai
 from datetime import datetime
+import jieba
+from pypinyin import lazy_pinyin, Style
+import logging
 
 app = Flask(__name__)
 
 # Set API key
-openai.api_key = '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'
+openai.api_key = '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'
 
 messages = [{"role": "system", "content": "You are a helpful assistant."}]
+
+def write_pinyin(chinese_text):
+    logging.getLogger('jieba').setLevel(logging.ERROR)
+
+    # Use jieba to segment the Chinese text
+    segments = jieba.lcut(chinese_text)
+
+    # TONE is used to add tone marks
+    return ' '.join([''.join(lazy_pinyin(segment, style=Style.TONE)) for segment in segments])
 
 @app.route('/')
 def index():
@@ -18,22 +30,14 @@ def ask():
     user_input = request.form['user_message']
     translation, pinyin = True, True
 
-    # Check for pinyin command
-    if 'no pinyin' in user_input.lower():
-        pinyin = False
+    if 'toggle pinyin' in user_input.lower():
+        pinyin = not pinyin
 
-    # Check for translation command
-    if 'no translation' in user_input.lower():
-        translation = False
+    if 'toggle translation' in user_input.lower():
+        translation = not translation
 
-    # Construct the model's instruction
     instruction = "Please respond in Simplified Mandarin."
-    if pinyin:
-        instruction += " Include pinyin in a separate new line."
-    if translation:
-        instruction += " Include an English translation in a separate new line."
 
-    # Get GPT-3 response
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=messages + [{"role": "user", "content": user_input + f' ({instruction})'}]
@@ -42,11 +46,8 @@ def ask():
     answer = response.choices[0].message['content']
     messages.append({"role": "assistant", "content": answer})
 
-    # Split the answer for better presentation if necessary
-    split_answers = answer.split('\n')
-    formatted_answer = '<br>'.join(split_answers)
+    formatted_answer = "中文: " + answer + "\nPīnyīn: " + write_pinyin(answer) 
 
-    # Add timestamp (this can also be done frontend, but here's the backend implementation)
     timestamp = datetime.now().strftime('%H:%M:%S')
     return jsonify({'response': formatted_answer, 'timestamp': timestamp})
 
