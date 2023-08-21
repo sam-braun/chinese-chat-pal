e964ef1b6d8c2e5ad2eee912a685abf206932975
Samuel Braun <slb2250@columbia.edu>
2023-08-20 23:37:17 -0400

    added modal

--- gpt3.5-chatbot/static/script.js
+++ gpt3.5-chatbot/static/script.js
@@ -65,3 +65,35 @@ function addNote() {
         document.getElementById("userNote").value = ""; // Clear the note box
     }
 }
+
+// modal
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// Get the start chatting button
+var startChatting = document.getElementById("startChatting");
+
+// When the page loads, open the modal 
+window.onload = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks on "Start Chatting", close the modal
+startChatting.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
